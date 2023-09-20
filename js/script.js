619898355b978e11c08caf83d686d11bacb9bbeb
Gabriel Rodrigues <rodriguesgasoares@gmail.com>
2023-09-20 18:09:44 -0300

    Criando a página de contato

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Fazer a troca de icones para o mobile
+
 const hamburguer = document.querySelector(".menu-hamburguer")
 
 hamburguer.addEventListener("click", function() {
@@ -11,3 +13,32 @@ hamburguer.addEventListener("click", function() {
         document.querySelector(".icone").src = "../img/icons/fechar.svg"
     }
 })
+
+// Ativar as perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+perguntas.forEach(pergunta => {
+    pergunta.addEventListener("click", event => {
+        const perguntaClicada = event.currentTarget;
+        const controls = perguntaClicada.getAttribute("aria-controls");
+        const resposta = document.getElementById(controls);
+
+        document.querySelectorAll('.perguntas dd.ativa').forEach(item => {
+            if (item.id !== controls) {
+                item.classList.remove("ativa");
+                const relatedButton = document.querySelector(`[aria-controls="${item.id}"]`);
+                relatedButton.setAttribute("aria-expanded", "false");
+            }
+        });
+
+        resposta.classList.toggle("ativa");
+        const ativa = resposta.classList.contains("ativa");
+        perguntaClicada.setAttribute("aria-expanded", ativa); 
+    });
+});
+
+
+
+
+
+
